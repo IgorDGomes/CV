7250a5743fc7e86587c076b6dde57726f74d7f0d
IgorDGomes <igordgomes@gmail.com>
2025-02-08 23:58:43 +0000

    fix: loading animation color, not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 
-export default function ErrorPage() {
+export default function NotFound() {
   return (
-    <main className="flex flex-col h-screen items-center justify-center gap-4 text-foreground">
+    <main className="flex flex-col h-screen items-center justify-center gap-4 text-background bg-foreground">
       <h1 className="text-4xl font-mono font-semibold">Not Found</h1>
       <p className="text-xl font-mono">Could not find requested resource</p>
       <Link href="/" className="underline underline-offset-2 font-mono text-lg">
